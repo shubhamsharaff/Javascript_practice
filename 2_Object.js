4bead9874f8ff3b792b544800e8b5a3bc22ac41d
Shubham Sharaff <90000866+shubhamsharaff@users.noreply.github.com>
2022-10-22 18:57:28 +0530

    Add files via upload
    Object content printing

---
+++ 2_Object.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// Sample object:
+// Sample Output: name,sclass,rollno
+
+let student = {
+  name: 'David Rayy',
+  sclass: 'VI',
+  rollno: 12,
+};
+
+console.log(`${student.name} ${student.sclass} ${student.rollno}`)
